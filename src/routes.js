c6ccab4ed6988823fa7e74833a8e9257533c7bfd
Ariane Sousa <ariane.sousa011@gmail.com>
2020-07-07 22:33:03 -0300

    criação da home controller

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const HomeController = require('./controllers/HomeController');
 const MotoristaController = require('./controllers/MotoristaController');
 
 const routes = express.Router();
 
+routes.get('/', HomeController.index);
 routes.get('/motoristas', MotoristaController.index);
 routes.get('/motoristas/:id', MotoristaController.show);
 routes.post('/motoristas', MotoristaController.create);
